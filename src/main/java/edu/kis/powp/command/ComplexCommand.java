d64a7bd9c659fda2d613381763470d6e776cdf86
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-04-22 13:56:34 +0200

    Added ComplexCommand class

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,16 @@
+package edu.kis.powp.command;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+    List<DriverCommand> driverCommandList;
+
+    public ComplexCommand(List<DriverCommand> driverCommandList) {
+        this.driverCommandList = driverCommandList;
+    }
+
+    @Override
+    public void execute() {
+        driverCommandList.forEach(DriverCommand::execute);
+    }
+}
